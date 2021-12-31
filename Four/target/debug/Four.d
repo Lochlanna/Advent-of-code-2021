a493aac54b19c90235456d3f9523e70391864d19
Lochlann Andrews <lochlanna@gmail.com>
2021-12-31 10:17:08 +1000

    removed target

--- Four/target/debug/Four.d
+++
@@ -1,1 +0,0 @@
-/Users/lochlanna/development/AdventOfCode/Four/target/debug/Four: /Users/lochlanna/development/AdventOfCode/Four/src/main.rs
--- Four/target/debug/build/memchr-f014d3bf242d433f/build_script_build-f014d3bf242d433f.d
+++
@@ -1,5 +0,0 @@
-/Users/lochlanna/development/AdventOfCode/Four/target/debug/build/memchr-f014d3bf242d433f/build_script_build-f014d3bf242d433f: /Users/lochlanna/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.4.1/build.rs
-
-/Users/lochlanna/development/AdventOfCode/Four/target/debug/build/memchr-f014d3bf242d433f/build_script_build-f014d3bf242d433f.d: /Users/lochlanna/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.4.1/build.rs
-
-/Users/lochlanna/.cargo/registry/src/github.com-1ecc6299db9ec823/memchr-2.4.1/build.rs:
