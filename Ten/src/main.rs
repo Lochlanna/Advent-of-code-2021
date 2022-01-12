4a79bf21f17c29f2a1ebeaa376e7d9fc91de4f41
Lochlann Andrews <lochlanna@gmail.com>
2022-01-12 21:44:20 +1000

    Day 10 cleanup

--- Ten/src/main.rs
+++ Ten/src/main.rs
@@ -1,15 +1,10 @@
 use std::collections::VecDeque;
 
 fn read_input(filename:&str) -> Vec<String> {
-    let file_contents = match std::fs::read_to_string(filename) {
-        Ok(fc) => fc,
+    match std::fs::read_to_string(filename) {
+        Ok(fc) => fc.lines().map(|l| l.to_string()).collect(),
         Err(_) => panic!("Couldn't read the input file")
-    };
-    let mut lines = Vec::new();
-    for line in file_contents.lines() {
-        lines.push(line.to_string());
     }
-    lines
 }
 
 fn get_opposite(c: char) -> char {
@@ -30,14 +25,9 @@ fn get_line_score(line: &String) -> Result<u64,u64> {
     let mut openers = VecDeque::new();
     for c in line.chars() {
         match c {
-            '{' | '(' | '[' | '<' => {
-                //push onto the Q
-                openers.push_front(c);
-            },
+            '{' | '(' | '[' | '<' => openers.push_front(c),
             '}' | ')' | ']' | '>' => {
-                //pop off the Q
-                let opener = openers.pop_front();
-                match opener {
+                match openers.pop_front() {
                     None => return Err(char_to_corrupt_score(c)),
                     Some(opener) => {
                         if opener != get_opposite(c) {
@@ -53,9 +43,7 @@ fn get_line_score(line: &String) -> Result<u64,u64> {
     }
     let mut repair_score:u64 = 0;
     while let Some(opener) = openers.pop_front() {
-        let opposite = get_opposite(opener);
-        repair_score = repair_score * 5;
-        repair_score += match opposite {
+        repair_score = repair_score * 5 + match get_opposite(opener) {
             ')' => 1,
             ']' => 2,
             '}' => 3,
