fb5b26dc586d5048e542942957897f5ed216f551
Lochlann Andrews <lochlann.b.andrews@boeing.com>
2022-01-19 16:32:38 +1000

    Started day 13

---
+++ thirteen/src/main.rs
@@ -0,0 +1,123 @@
+use std::fmt::{Display, Formatter};
+use crate::table::{Table, TableCell, TableIterator, TableIteratorType, IteratorDirection};
+
+mod table;
+
+trait Foldable {
+    /// Folds the structure around the provided x axis
+    /// The structure will be folded left to right around the axis
+    /// The line along which the struct is folded will no longer exist after this operation
+    /// The original table is modified to perform this operation
+    ///
+    /// # Arguments
+    ///
+    /// * `x`: The index of the column to fold around. This column will no longer exist after the function is complete
+    ///
+    /// returns: ()
+    fn fold_x(&mut self, x:usize);
+    fn fold_y(&mut self, y:usize);
+}
+
+impl<T: Copy> Table<T> {
+    fn fold(&mut self, mut src_iter: TableIterator, mut dst_iter: TableIterator) {
+        loop {
+            let src_value:T = match src_iter.next(self) {
+                None => break, // There is no more to iterate over
+                Some(src_value) => {
+                    match src_value {
+                        TableCell::Null => { // empty cell so we skip it on the destination side
+                            if !dst_iter.skip(self) {
+                                break; // no more space on the destination
+                            }
+                            continue; // We have skipped here so we need to go back to the start of the loop
+                        },
+                        TableCell::Some(src_value) => src_value.clone()
+                    }
+                }
+            };
+            if !dst_iter.update(self, src_value) {
+                break;
+            }
+        }
+    }
+}
+
+impl<T: Copy> Foldable for table::Table<T> {
+
+    fn fold_x(&mut self, x: usize) {
+        if x + 1 >= self.num_columns() {
+            // Can't fold along a row that doesn't exist or the last row
+            return;
+        }
+        let src_iter = table::TableIterator::new(x+1, IteratorDirection::Forward, TableIteratorType::Column);
+        let dst_iter = table::TableIterator::new(x-1, IteratorDirection::Backward, TableIteratorType::Column);
+
+        self.fold(src_iter, dst_iter);
+        self.truncate_by_column(x);
+    }
+
+    fn fold_y(&mut self, y: usize) {
+        if y + 1 >= self.num_rows() {
+            // Can't fold along a row that doesn't exist or the last row
+            return;
+        }
+        let src_iter = table::TableIterator::new(y+1, IteratorDirection::Forward, TableIteratorType::Row);
+        let dst_iter = table::TableIterator::new(y-1, IteratorDirection::Backward, TableIteratorType::Row);
+
+        self.fold(src_iter, dst_iter);
+        self.truncate_by_row(y);
+    }
+}
+
+impl Display for table::Table<bool> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut table_string = String::with_capacity(self.len() + self.num_rows());
+        for row in self.row_iterator() {
+            for column in row {
+                match column {
+                    TableCell::Null => table_string.push('.'),
+                    TableCell::Some(_) => {table_string.push('#')}
+                }
+            }
+            table_string.push('\n');
+        }
+        table_string.pop();//pop final new line
+        f.write_str(table_string.as_str())
+    }
+}
+
+fn read_input(filename: &str) -> Table<bool> {
+    let file_contents = match std::fs::read_to_string(filename) {
+        Ok(fc) => fc,
+        Err(_) => panic!("Couldn't read the input")
+    };
+    let mut points:Vec<(usize, usize)> = Vec::new();
+    let mut max_x = 0;
+    let mut max_y = 0;
+    for line in file_contents.lines() {
+        if line.is_empty() {
+            break;
+        }
+        let parts: Vec<&str> = line.split(',').collect();
+        if parts.len() != 2 {
+            panic!("Invalid point while reading input");
+        }
+        let x = parts[0].parse::<usize>().expect("Couldn't parse point while reading input");
+        let y = parts[1].parse::<usize>().expect("Couldn't parse point while reading input");
+        points.push((x,y));
+        max_x = std::cmp::max(x, max_x);
+        max_y = std::cmp::max(y, max_y);
+    }
+    let mut table = Table::with_capacity(max_x, max_x * max_y);
+    for (x, y) in points {
+        table.set_cell(x,y,true);
+    }
+    table
+}
+
+fn main() {
+    let mut table = read_input("input_small");
+    println!("Read input table \n{}", table);
+    table.fold_y(7);
+    println!("Fold on 7 \n{}", table);
+}
---
+++ thirteen/src/table/mod.rs
@@ -0,0 +1,237 @@
+use std::slice::Chunks;
+
+pub enum TableCell<T> {
+    Null,
+    Some(T)
+}
+
+impl<T: Copy + Clone> Clone for TableCell<T> {
+    fn clone(&self) -> Self {
+        match self {
+            TableCell::Null => TableCell::Null,
+            TableCell::Some(v) => TableCell::Some(v.clone())
+        }
+    }
+}
+
+impl<T:Copy> Copy for TableCell<T> {
+}
+
+impl<T> Default for TableCell<T> {
+    fn default() -> Self {
+        TableCell::Null
+    }
+}
+
+pub struct Table<T> {
+    data: Vec<TableCell<T>>,
+    row_size: usize
+}
+
+impl <T: Copy> Table<T> {
+    // This is kinda pricey
+    pub fn truncate_by_column(&mut self, col: usize) {
+        let mut new_data_table = Vec::with_capacity(self.num_rows() * col);
+        for value in self.data.chunks(col).step_by(2) {
+            new_data_table.copy_from_slice(value);
+        }
+        self.row_size = col;
+        self.data = new_data_table;
+    }
+}
+
+impl<T> Table<T> {
+    pub fn new(row_size: usize) -> Table<T> {
+        Table {
+            data: vec![],
+            row_size
+        }
+    }
+
+    pub fn with_capacity(row_size: usize, capacity: usize) -> Table<T> {
+        let mut table = Table::new(row_size);
+        table.data.resize_with(capacity, Default::default);
+        table
+    }
+
+    pub fn row_iterator(&self) -> Chunks<'_, TableCell<T>> {
+        self.data.chunks(self.row_size)
+    }
+
+    //mmm cheap
+    pub fn truncate_by_row(&mut self, row: usize) {
+        let start_of_row = row * self.row_size;
+        self.data.truncate(start_of_row);
+    }
+
+    pub fn set_cell(&mut self, x: usize, y:usize, value: T) {
+        let insert_index = y * self.row_size + x;
+        self.set_cell_by_index(insert_index, value);
+    }
+
+    pub fn set_cell_by_index(&mut self, index: usize, value: T) {
+        if self.data.len() < index+1 {
+            self.data.resize_with(index+1, Default::default)
+        }
+        self.data[index] = TableCell::Some(value);
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> Option<&TableCell<T>> {
+        let index = y * self.row_size + x;
+        self.get_index(index)
+    }
+
+    pub fn get_mut(&mut self, x:usize, y:usize) -> Option<&mut TableCell<T>> {
+        let index = y * self.row_size + x;
+        self.get_index_mut(index)
+    }
+
+    pub fn num_rows(&self) -> usize {
+        (self.data.len() as f64 / self.row_size as f64).ceil() as usize
+    }
+
+    pub fn num_columns(&self) -> usize {
+        self.row_size
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn get_index_mut(&mut self, index: usize) -> Option<&mut TableCell<T>> {
+        match self.data.get_mut(index) {
+            None => None,
+            Some(value) => Some(value),
+        }
+    }
+
+    fn get_index(&self, index: usize) -> Option<&TableCell<T>> {
+        match self.data.get(index) {
+            None => None,
+            Some(value) => Some(value),
+        }
+    }
+}
+pub enum IteratorDirection{
+    Forward,
+    Backward
+}
+
+pub enum TableIteratorType{
+    Row,
+    Column
+}
+
+pub struct TableIterator {
+    current_row:usize,
+    current_col:usize,
+    direction: IteratorDirection,
+    iterator_type: TableIteratorType
+}
+
+pub enum UpdateStatus {
+    Updated,
+    NotUpdated,
+    Failed
+}
+
+impl TableIterator {
+    pub fn new(start_row:usize, direction: IteratorDirection, iterator_type: TableIteratorType) -> TableIterator {
+        TableIterator {
+            current_row: start_row,
+            current_col: 0,
+            direction,
+            iterator_type
+        }
+    }
+
+    fn increment_column<T>(&mut self, table: &Table<T>) -> Option<usize> {
+        if self.current_row > table.num_rows() {
+            self.current_row = 0;
+            match self.direction {
+                IteratorDirection::Forward => self.current_col += 1,
+                IteratorDirection::Backward => {
+                    if self.current_col > 0 {
+                        self.current_col += 1;
+                    } else {
+                        return None
+                    }
+                }
+            };
+        }
+        let current_index = self.current_row * table.row_size + self.current_col;
+        self.current_row += 1;
+        if current_index > table.len() {
+            return None;
+        }
+        Some(current_index)
+    }
+
+    fn increment_row<T>(&mut self,table: &Table<T>) -> Option<usize> {
+        if self.current_col > table.row_size {
+            self.current_col = 0;
+            match self.direction {
+                IteratorDirection::Forward => self.current_row += 1,
+                IteratorDirection::Backward => {
+                    if self.current_row > 0 {
+                        self.current_row -= 1;
+                    } else {
+                        return None;
+                    }
+                }
+            }
+        }
+        let current_index = self.current_row * table.row_size + self.current_col;
+        self.current_col += 1;
+        if current_index > table.len() {
+            return None;
+        }
+        Some(current_index)
+    }
+
+    fn increment<T>(&mut self,table: &Table<T>) -> Option<usize> {
+        match self.iterator_type {
+            TableIteratorType::Row => self.increment_row(table),
+            TableIteratorType::Column => self.increment_column(table)
+        }
+    }
+
+    pub fn next<'a, T>(&mut self, table: &'a Table<T>) -> Option<&'a TableCell<T>> {
+        match self.increment(table) {
+            None => None,
+            Some(index) => table.get_index(index)
+        }
+    }
+
+    pub fn update<T>(&mut self, table: &mut Table<T>, new_value: T) -> bool {
+        match self.increment(table) {
+            None => false,
+            Some(index) => {
+                table.set_cell_by_index(index, new_value);
+                true
+            }
+        }
+    }
+
+    pub fn skip<T>(&mut self,  table: &Table<T>) -> bool {
+        self.increment(table).is_some()
+    }
+
+    pub fn update_if<T>(&mut self,  table: &mut Table<T>, new_value: T, test:fn(&TableCell<T>)->bool) -> UpdateStatus {
+        match self.increment(table) {
+            None => UpdateStatus::Failed,
+            Some(index) => {
+                match table.get_index(index) {
+                    None => return UpdateStatus::Failed,
+                    Some(cell) => {
+                        if !test(cell) {
+                            return UpdateStatus::NotUpdated;
+                        }
+                    }
+                }
+                table.set_cell_by_index(index, new_value);
+                UpdateStatus::Updated
+            }
+        }
+    }
+}
