d5cb713656b01a16f762875c0ddecdd82eb786e4
Lochlann Andrews <lochlanna@gmail.com>
2022-01-20 19:12:16 +1000

    day fifteen optimisations

--- fifteen/src/main.rs
+++ fifteen/src/main.rs
@@ -2,9 +2,8 @@ use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::env;
 use std::fmt::{Display, Formatter};
-use std::ops::Add;
 use std::time::Instant;
-use ndarray::{Array2, ArrayView};
+use ndarray::Array2;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 
@@ -13,14 +12,14 @@ enum Direction {
     Left,
     Right,
     Up,
-    Down
+    Down,
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Node {
     distance: usize,
     risk: u8,
-    searched: bool
+    searched: bool,
 }
 
 impl Default for Node {
@@ -30,18 +29,11 @@ impl Default for Node {
 }
 
 impl Node {
-    fn new(risk:u8) -> Self {
+    fn new(risk: u8) -> Self {
         Node {
             distance: usize::MAX,
             risk,
-            searched: false
-        }
-    }
-    fn clone_with_risk(&self, risk:u8) -> Self {
-        Node {
-            distance: self.distance,
-            risk,
-            searched: false
+            searched: false,
         }
     }
 }
@@ -50,7 +42,7 @@ impl Node {
 struct SearchToken {
     x: usize,
     y: usize,
-    distance: u32
+    distance: u32,
 }
 
 impl SearchToken {
@@ -58,6 +50,7 @@ impl SearchToken {
         SearchToken { x, y, distance }
     }
 }
+
 impl Eq for SearchToken {}
 
 impl PartialOrd<Self> for SearchToken {
@@ -73,7 +66,7 @@ impl Ord for SearchToken {
 }
 
 struct Map {
-    data: ndarray::Array2<Node>
+    data: ndarray::Array2<Node>,
 }
 
 impl Display for Map {
@@ -94,19 +87,19 @@ impl Display for Map {
 
 
 impl Map {
-    fn new(data: Vec<Node>, n_rows:usize, n_cols:usize) -> Self {
+    fn new(data: Vec<Node>, n_rows: usize, n_cols: usize) -> Self {
         Map {
-            data: Array2::from_shape_vec((n_rows,n_cols), data).expect("Couldn't create array")
+            data: Array2::from_shape_vec((n_rows, n_cols), data).expect("Couldn't create array")
         }
     }
     fn search(&mut self, start_x: usize, start_y: usize, end_x: usize, end_y: usize) -> Option<usize> {
         let start_node = self.get_mut(start_x, start_y).expect("couldn't get the start node");
         start_node.distance = 0;
-        let mut path_q:BinaryHeap<SearchToken> = BinaryHeap::with_capacity(10000);
+        let mut path_q: BinaryHeap<SearchToken> = BinaryHeap::with_capacity(10000);
         path_q.push(SearchToken::new(start_x, start_y, 0));
         while let Some(closest_token) = path_q.pop() {
             let current_node = self.get_mut(closest_token.x, closest_token.y).expect("Couldn't find node for token");
-            if closest_token.x == end_x-1 && closest_token.y == end_y-1 {
+            if closest_token.x == end_x - 1 && closest_token.y == end_y - 1 {
                 return Some(current_node.distance); // we have found the shortest path
             }
             current_node.searched = true;
@@ -118,7 +111,7 @@ impl Map {
                         if !node.searched && node.distance >= current_dist + node.risk as usize {
                             node.distance = current_dist + node.risk as usize;
                             let distance_to_end = (((x as i32 - end_x as i32).pow(2) + (y as i32 - end_y as i32).pow(2)) as f32).sqrt();
-                            let new_token = SearchToken::new(x,y,distance_to_end as u32 + node.distance as u32);
+                            let new_token = SearchToken::new(x, y, distance_to_end as u32 + node.distance as u32);
                             path_q.push(new_token);
                         }
                     }
@@ -128,11 +121,11 @@ impl Map {
         None
     }
 
-    fn get_mut(&mut self, x:usize, y: usize) -> Option<&mut Node> {
-        self.data.get_mut((y,x))
+    fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut Node> {
+        self.data.get_mut((y, x))
     }
 
-    fn get_by_direction(&mut self, current_x: usize, current_y: usize, direction:Direction) -> Option<(&mut Node, usize, usize)> {
+    fn get_by_direction(&mut self, current_x: usize, current_y: usize, direction: Direction) -> Option<(&mut Node, usize, usize)> {
         let mut x = current_x;
         let mut y = current_y;
         match direction {
@@ -140,86 +133,102 @@ impl Map {
                 if current_x <= 0 {
                     return None;
                 }
-                x-=1;
+                x -= 1;
             }
-            Direction::Right => x+=1,
+            Direction::Right => x += 1,
             Direction::Up => {
                 if current_y <= 0 {
                     return None;
                 }
-                y-=1;
+                y -= 1;
             }
-            Direction::Down => y+=1
+            Direction::Down => y += 1
         }
         self.get_mut(x, y).and_then(|n| Some((n, x, y)))
     }
 
-    fn dimensions(&self)->(usize,usize) {
+    fn dimensions(&self) -> (usize, usize) {
         if self.data.is_empty() {
-            return (0,0);
+            return (0, 0);
         }
         (self.data.ncols(), self.data.nrows())
     }
 
-    fn tile(&mut self, x:usize, y:usize) {
+    fn tile(&mut self, x: usize, y: usize) {
         let original_num_rows = self.data.nrows();
         let original_num_columns = self.data.ncols();
-        let mut new_data = Vec::with_capacity(original_num_rows*y *original_num_columns*x);
-        for v in 0..(original_num_rows*y) {
-            for h in 0..original_num_columns*x {
+        let mut new_arr= Array2::<Node>::default((original_num_rows * y, original_num_columns * x));
+        let mut new_arr_iter = new_arr.iter_mut();
+        let mut row_iterator = self.data.rows().into_iter();
+        for v in 0..(original_num_rows * y) {
+            let row = match row_iterator.next() {
+                None => {
+                    row_iterator = self.data.rows().into_iter();
+                    row_iterator.next().expect("couldn't get a row after resetting the iterator")
+                },
+                Some(row) => row
+            };
+            let mut col_iterator = row.iter();
+            for h in 0..original_num_columns * x {
                 let vertical_addition = v / original_num_rows; // int division rounds down
                 let horizontal_addition = h / original_num_columns;
-                let x = h % original_num_columns;
-                let y = v % original_num_rows;
-                let node = self.data.get((y,x)).expect("Couldn't get node from original data");
+                let node = match col_iterator.next() {
+                    None => {
+                        col_iterator = row.iter();
+                        col_iterator.next().expect("couldn't get col after resetting the iterator")
+                    }
+                    Some(node) => node
+                };
                 let mut new_risk = node.risk as usize + vertical_addition + horizontal_addition;
                 if new_risk >= 10 {
-                    new_risk = (new_risk%10) + 1;
+                    new_risk = (new_risk % 10) + 1;
                 }
-                new_data.push(node.clone_with_risk(new_risk as u8));
+                new_arr_iter.next().expect("Couldn't get value in new array").risk = new_risk as u8;
             }
+
         }
-        self.data = Array2::from_shape_vec((original_num_rows*y, original_num_columns*x), new_data).expect("Couldn't create arr2 from flattened")
+        self.data = new_arr;
     }
 }
 
-fn read_input(filename:&str) -> Map {
+fn read_input(filename: &str) -> Map {
     let file_contents = std::fs::read_to_string(filename).expect("couldn't read file");
     let mut data = Vec::new();
     let mut width = 0;
     let mut row_count = 0;
-    for line in file_contents.lines(){
+    for line in file_contents.lines() {
         for c in line.chars() {
             let parsed = c.to_digit(10).expect("couldn't parse character to digit") as u8;
             data.push(Node::new(parsed));
         }
         width = line.len();
-        row_count+= 1;
+        row_count += 1;
     }
     Map::new(data, row_count, width)
 }
+
 fn problem_one() {
     let now = Instant::now();
-    let args:Vec<String> = env::args().collect();
-    let mut map = read_input(if args.len() != 2 {"input"} else {args[1].as_str()});
+    let args: Vec<String> = env::args().collect();
+    let mut map = read_input(if args.len() != 2 { "input" } else { args[1].as_str() });
     let (end_x, end_y) = map.dimensions();
 
-    if let Some(distance) = map.search(0,0, end_x, end_y) {
+    if let Some(distance) = map.search(0, 0, end_x, end_y) {
         println!("distance was {}", distance);
     } else {
         println!("couldn't find a path");
     }
     let dur = now.elapsed();
     println!("time taken was {} seconds or {} microseconds", dur.as_secs_f64(), dur.as_micros());
-
 }
+
 fn problem_two() {
     let now = Instant::now();
-    let args:Vec<String> = env::args().collect();
-    let mut map= read_input(if args.len() != 2 {"input"} else {args[1].as_str()});
-    map.tile(5,5);
+    let args: Vec<String> = env::args().collect();
+    let mut map = read_input(if args.len() != 2 { "input" } else { args[1].as_str() });
+    map.tile(5, 5);
     let (end_x, end_y) = map.dimensions();
-    if let Some(distance) = map.search(0,0, end_x, end_y) {
+    if let Some(distance) = map.search(0, 0, end_x, end_y) {
         println!("distance was {}", distance);
     } else {
         println!("couldn't find a path");
